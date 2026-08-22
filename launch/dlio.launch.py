8794b39d2d6a5d43bc01bff74e6ed32e9316be87
Laksh Nanwani <lakshanshul@gmail.com>
2026-08-23 04:31:35 +0530

    one trial success

--- launch/dlio.launch.py
+++ launch/dlio.launch.py
@@ -73,6 +73,7 @@ def generate_launch_description():
         parameters=[dlio_yaml_path, dlio_params_yaml_path],
         remappings=[
             ('keyframes', 'dlio/odom_node/pointcloud/keyframe'),
+            ('map', 'dlio_map'),
         ],
     )
 
